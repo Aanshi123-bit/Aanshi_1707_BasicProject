2d07b63382121bc55e8e6f1965d03b397cb6702d
Aanshi <aanshisrivastava1707@gmail.com>
2026-07-14 11:09:38 +0530

    modify app.js

--- app.js
+++ app.js
@@ -6,18 +6,28 @@ const session = require("express-session");
 
 const connectDB = require("./config/db");
 
+// =======================
+// Route Imports
+// =======================
+
 const authRoutes = require("./routes/auth");
 const dashboardRoutes = require("./routes/dashboard");
 const taskRoutes = require("./routes/task");
 const charmRoutes = require("./routes/charm");
+const leaderboardRoutes = require("./routes/leaderboard");
 
 const app = express();
 
+// =======================
 // Connect Database
+// =======================
+
 connectDB();
 
-
+// =======================
 // Middlewares
+// =======================
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -31,30 +41,62 @@ app.use(
     })
 );
 
+// =======================
 // View Engine
+// =======================
+
 app.set("view engine", "ejs");
 
-
+// =======================
 // Routes
+// =======================
+
+// Landing Page
+
 app.get("/", (req, res) => {
+
     res.render("index");
+
 });
+
+// Authentication Routes
 
 app.use("/", authRoutes);
+
+// Dashboard Routes
+
 app.use("/", dashboardRoutes);
+
+// Study Planner Routes
+
 app.use("/", taskRoutes);
+
+// Charms Routes
+
 app.use("/", charmRoutes);
 
+// Leaderboard Routes
 
-// 404 Page
+app.use("/", leaderboardRoutes);
+
+// =======================
+// 404 Route
+// =======================
+
 app.use((req, res) => {
+
     res.status(404).send("404 | Page Not Found");
+
 });
 
-
+// =======================
 // Server
+// =======================
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
+
     console.log(`🚀 Server running on http://localhost:${PORT}`);
+
 });
